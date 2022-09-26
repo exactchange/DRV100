5a1fec1d16164e7aee0cf220ffbc2974c887b53d
exactchange <benny@exactchange.network>
2022-09-26 14:53:12 -0700

    add comments

--- index.js
+++ index.js
@@ -8,6 +8,14 @@
 const SUCCESS_CODE = 200;
 
 module.exports = ({ drv, peers, serviceEvents }) => {
+
+  /*
+   * Define a contract that either transfers DRV from
+   * a sender to a recipient, or records collateral (like USD)
+   * from a recipient, then recursively transfers DRV
+   * against it.
+   */
+
   const Contract = async ({
     token,
     sender,
@@ -18,6 +26,12 @@ module.exports = ({ drv, peers, serviceEvents }) => {
     drvValue,
     isDrv
   }) => {
+
+    /*
+     * If DRV is being transferred, send a normal POST request
+     * to `drv-core` and return the result.
+     */
+
     if (isDrv) {
       const transactionResult = await serviceEvents.onServicePost({
         service: drv,
@@ -40,6 +54,12 @@ module.exports = ({ drv, peers, serviceEvents }) => {
       return transactionResult;
     }
 
+    /*
+     * If non-DRV is being transferred, send a GET request
+     * to `drv-core` to make sure the DRV is tradable.
+     * (hit the /price endpoint)
+     */
+
     const priceResult = await serviceEvents.onServiceGet({
       service: drv,
       serviceName: '/',
@@ -49,6 +69,11 @@ module.exports = ({ drv, peers, serviceEvents }) => {
     if (!priceResult || priceResult.status !== SUCCESS_CODE) {
       return false;
     }
+
+    /*
+     * If it has a valid price, recursively call this contract
+     * transferring the originally requested DRV amount.
+     */
 
     const transferResult = await Contract({
       token,
